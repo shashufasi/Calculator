eb66a17516ceba900f7819cc61eb34bfbe467682
Faisal Qazi <faisalqazi31@gmail.com>
2018-09-27 20:37:10 +0500

    Commit

--- app/src/main/java/com/example/shashu/calculator/MainActivity.java
+++ app/src/main/java/com/example/shashu/calculator/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.shashu.calculator;
 
+import android.widget.Button;
+import android.view.View;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
-
+private Button btnOpenCalculator;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnOpenCalculator=findViewById(R.id.btnCalculator);
+        btnOpenCalculator.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view)
+            {
+                Intent intent=new Intent(MainActivity.this,CalculatorActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
